2dc353d1cab1d7b7e15b68e83f111b195c6394c9
alxmpotter <alxmpotter@gmail.com>
2017-11-06 09:42:10 -0600

    commit view exercise from friday

--- src/main/java/com/codeup/blog/controllers/PostsController.java
+++ src/main/java/com/codeup/blog/controllers/PostsController.java
@@ -1,33 +1,51 @@
 package com.codeup.blog.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.ArrayList;
+
 @Controller
 public class PostsController {
 
-    @ResponseBody
+
     @GetMapping("/posts")
-    public String posts(){
-        return "posts index page";
+    public String showAll(Model viewModel){
+
+        ArrayList<Post> posts = new ArrayList<>();
+
+        posts.add(new Post("Example 1", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque commodi eligendi necessitatibus voluptates. At distinctio dolores minus molestiae mollitia nemo sapiente ut veniam voluptates! Corporis distinctio error quaerat vel!"));
+
+        posts.add(new Post("Example 2", "QWE Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque commodi eligendi necessitatibus voluptates. At distinctio dolores minus molestiae mollitia nemo sapiente ut veniam voluptates! Corporis distinctio error quaerat vel!"));
+
+
+
+        viewModel.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
-    @ResponseBody
+
+
     @GetMapping("/posts/{id}")
-    public String singlePost(){
-        return "view individual post";
+    public String singlePost(@PathVariable int id, Model viewModel){
+        Post post = new Post("Example 1", "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci atque commodi eligendi necessitatibus voluptates. At distinctio dolores minus molestiae mollitia nemo sapiente ut veniam voluptates! Corporis distinctio error quaerat vel!");
+
+        viewModel.addAttribute("post", post);
+        return "posts/show";
     }
 
-    @ResponseBody
+
     @GetMapping("/posts/create")
     public String createPostForm(){
         return "view the form for creating a post";
     }
 
-    @ResponseBody
+
     @PostMapping("/posts/create")
     public String createPost(){
         return "create a new post";
